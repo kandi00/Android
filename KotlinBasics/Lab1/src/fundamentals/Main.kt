cc9ece3bfde2b76132f24cbf185941beaf174399
kandi00 <kovacs.andrea.home@gmail.com>
2021-09-24 20:47:34 +0300

    Házi leadása

--- KotlinBasics/Lab1/src/fundamentals/Main.kt
+++ KotlinBasics/Lab1/src/fundamentals/Main.kt
@@ -1,22 +1,63 @@
 package fundamentals
 
-fun main(args: Array<String>) {
+import kotlin.random.Random
+
+fun main() {
+
     //1
     val x = 2
     val y = 3
     fun1(x, y)
+
     //2
-    fun2()
+    var daysOfWeek = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    fun2(daysOfWeek)
+
     //3
-    printPrime(1,20)
+    printPrimesWithinARange(1,20)
+    print("\n")
+
+    //4
+    var word = "abc"
+    word = messageCoding(word) {encode(it)}
+    println(word)
+    word = messageCoding(word) {decode(it)}
+    println(word)
+    //5
+
+    //Compact function = single-expression function
+    printEvenNumbers(listOf(1,2,3,4,5,6))
+    print("\n")
+
+    //6
+    fun6(listOf(1,2,3,4,5,6),daysOfWeek)
+
+    //7
+    daysOfWeek = daysOfWeek.toMutableList()
+    daysOfWeek.removeAll { it.contains("n") }
+    daysOfWeek.forEach{ print("$it ")}
+    print("\n")
+    daysOfWeek.withIndex().forEach{ println("Item at ${it.index} is ${it.value}") }
+    daysOfWeek.sortedBy { it }.forEach{ print("$it ") }
+    print("\n")
+
+    //8
+    val array  = List(10) { Random.nextInt(0, 100) }
+    array.forEach{ println("$it ")}
+    array.sortedBy { it }
+    if (array.filter { it % 2 == 0 }.isNotEmpty()) println("The array contains at least one even number!")
+    if (array.filter { it % 2 == 0 }.size == array.size) println("All the numbers are even!")
+    var sum = 0.0
+    array.forEach { sum+=it}
+    println("Average: ${sum/array.size}")
+
 }
 
 fun fun1(x: Int, y: Int) {
     println("$x + $y = ${x+y}")
 }
 
-fun fun2(){
-    val daysOfWeek = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+fun fun2(daysOfWeek : List<String>){
     for(day in daysOfWeek) print("$day ")
     print("\n")
     for(day in daysOfWeek.filter{ it.startsWith("T") }) print("$day ")
@@ -24,6 +65,7 @@ fun fun2(){
     for(day in daysOfWeek.filter { it.contains("e") }) print("$day ")
     print("\n")
     for(day in daysOfWeek.filter { it.length == 6 }) print("$day ")
+    print("\n")
 }
 
 fun isPrime(number : Int): Boolean {
@@ -38,7 +80,7 @@ fun isPrime(number : Int): Boolean {
     return flag
 }
 
-fun printPrime(nr1 : Int, nr2: Int){
+fun printPrimesWithinARange(nr1 : Int, nr2: Int){
     for(i in nr1..nr2) if( isPrime(i)) print("$i ")
 }
 
@@ -60,4 +102,20 @@ fun decode(word : String): (String) {
         word1 = "$word1${letter-1}"
     }
     return word1
+}
+
+fun printEvenNumbers(list : List<Int>) = list.filter { it % 2 == 0 }.forEach{print("$it ")}
+
+fun fun6(list : List<Int>, daysOfWeek : List<String>){
+    list.map { it * 2 }.forEach{ print("$it ") }
+    print("\n")
+    daysOfWeek.map { it.toUpperCase() }.forEach{ print("$it ") }
+    print("\n")
+    daysOfWeek.map { it[0].toLowerCase() }.forEach{ print("$it ") }
+    print("\n")
+    var sum = 0
+    daysOfWeek.forEach{ print("${it.length} "); sum += it.length }
+    print("\n")
+    print("The average length of days = ${sum/7.0}")
+    print("\n")
 }
