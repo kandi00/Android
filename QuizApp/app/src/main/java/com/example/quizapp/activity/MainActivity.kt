c566afcfc6f5d226a883d3c68b99f1ab2369cf8e
Kovács Andrea <79245705+kandi00@users.noreply.github.com>
2021-10-30 22:24:53 +0300

    txt, navigation drawer, recyclerview added

--- QuizApp/app/src/main/java/com/example/quizapp/activity/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/activity/MainActivity.kt
@@ -4,17 +4,42 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.quizapp.R
+import com.example.quizapp.data.SharedViewModel
 import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val tag = "MainActivity"
     private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: SharedViewModel
+    private lateinit var navController : NavController
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = SharedViewModel(application)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = binding.maDrawerLayout
+        binding.navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onStart(){
